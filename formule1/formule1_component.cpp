523532176a0254c56c802db9b37bcc8892828257
Ayrox99 <56259172+Ayrox99@users.noreply.github.com>
2021-03-08 02:49:27 +0100

    Update formule1_component.cpp

--- formule1/formule1_component.cpp
+++ formule1/formule1_component.cpp
@@ -23,8 +23,8 @@ struct Formula1 {
 namespace py = pybind11;
 
 PYBIND11_MODULE(formule1_component, m) {
-	py::class_<Formula1>(m, "Formula 1", py::dynamic_attr())
-		.def(py::init<const int&>())
+	py::class_<Formula1>(m, "Formula1", py::dynamic_attr())
+		.def(py::init<int>())
 		.def("setSpeed", &Formula1::setSpeed)
 		.def("getSpeed", &Formula1::getSpeed);
 }
